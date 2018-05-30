f4581a1509767c567c33686ca0447562e2741378
Felix <felix.herrenschmidt@isen.fr>
2018-05-30 16:27:21 +0200

    Changement design

---
+++ Stratego/src/Command/ChatServerCommandManual.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FelixMac
+ * Date: 28/05/2018
+ * Time: 16:03"
+ */
+
+
+namespace App\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Ratchet\Server\IoServer;
+use App\Server\Chat;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+class ChatServerCommandManual extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('felix:app:chat-server:manual')
+            ->setDescription('Start chat server');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $server = IoServer::factory(
+            new HttpServer(new WsServer(new Chat())),
+            8080,
+            '127.0.0.1'
+        );
+        $output->writeln("<info> [OK] Serveur lancé sur le port 8080</info>");
+        $server->run();
+        $output->writeln("Fermeture du serveur");
+    }
+}
